55531de133dc164579b1275287c7f0404f8243bd
lipeihan <lipeihan@star-net.cn>
2017-07-18 09:49:56 +0800

    FEATURE 新增step1

---
+++ step1/server.js
@@ -0,0 +1,40 @@
+const http = require('http');
+const path = require('path');
+const url = require('url');
+const fs = require('fs');
+
+
+function staticRoot(staticPath, req, res) {
+  let pathname = url.parse(req.url).pathname;
+
+
+  if (pathname === '/') {
+    pathname += 'index.html';
+  }
+
+  const filePath = path.join(staticPath, pathname);
+
+  // 用同步方法会阻塞，且错误时整个服务器会挂掉
+  /*
+    const file = fs.readFileSync(filePath,'binary');
+    res.write(file, 'binary');
+    res.end();
+  */
+
+  fs.readFile(filePath, 'binary', function(err, file) {
+    if (err) {
+      res.writeHead(404, 'not found');
+      res.end('<h1>404 Not Found</h1>');
+    } else {
+      res.write(file, 'binary');
+      res.end();
+    }
+  });
+}
+
+const server = http.createServer(function(req, res) {
+  staticRoot(path.resolve(__dirname, 'static'), req, res);
+});
+
+console.log('node server start~');
+server.listen(8888);
